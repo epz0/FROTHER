4df1773f2743d01e069c0090628798c535c8091f
eparavizo <ep650@cam.ac.uk>
2024-10-20 02:52:14 +0100

    add function to read output files

--- scripts/format_output.py
+++ scripts/format_output.py
@@ -1,3 +1,7 @@
+import os
+import pandas as pd
+import json
+
 def format_output(output):
     output = output.replace("\n", "")
     output = output.replace("    ", "")
@@ -13,3 +17,32 @@ def format_output(output):
         output = output.replace('"}', '"')
     output = output.split('|')
     return output
+
+def read_output(f_output):
+    files = []
+
+    for filename in os.listdir(f_output):
+        filePath = os.path.join(f_output, filename)
+
+        with open(filePath, 'r') as file:
+            content = file.read()
+
+        files.append(content)
+
+    return files
+
+def format_read_file(ls_js_read):
+    ls_all = []
+
+    for i in range(len(ls_js_read)):
+        ls_all_json=[]
+        tmp_json = ls_js_read[i].replace('\n',',').replace(' {','{')
+        tmp_json = tmp_json.replace('},{','}|{')
+        ls_all_json.append(tmp_json.split('|'))
+        for id in ls_all_json[0]:
+            ls_all.append(json.loads(id))
+
+    df = pd.DataFrame(ls_all)
+    df = df.sort_values(by='ID')
+
+    return ls_all, df
